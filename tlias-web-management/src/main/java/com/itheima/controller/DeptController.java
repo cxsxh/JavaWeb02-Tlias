fc52871d23d44064b4dac1f49dd031bc2c8f5a13
cxsxh <207433274@qq.com>
2024-12-18 19:42:57 +0800

    Tlias-JavaWeb

--- tlias-web-management/src/main/java/com/itheima/controller/DeptController.java
+++ tlias-web-management/src/main/java/com/itheima/controller/DeptController.java
@@ -4,8 +4,7 @@ import com.itheima.pojo.Dept;
 import com.itheima.pojo.Result;
 import com.itheima.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,10 +14,46 @@ public class DeptController {
     @Autowired
     private DeptService deptService;
 
+    /**
+     * 查询部门列表
+     */
 //    @RequestMapping(value = "/depts", method = RequestMethod.GET)
-    @GetMapping("depts")
+    @GetMapping("/depts")
     public Result list(){
         List<Dept> deptList = deptService.findAll();
         return Result.success(deptList);
     }
+
+    /**
+     * 删除部门
+     */
+    //方式1
+//    @DeleteMapping("depts")
+//    public Result delete(HttpServletRequest request){
+//        String idStr = request.getParameter("id");
+//        int id = Integer.parseInt(idStr);
+//        return Result.success();
+//    }
+
+    //方式2
+//    @DeleteMapping("depts")
+//    public Result delete(@RequestParam(value = "id", required = false) Integer deptId) {
+//        return Result.success();
+//    }
+
+    //方式3 （要保证前端请求参数名与服务器端方法的参数名一致）
+    @DeleteMapping("/depts")
+    public Result delete(Integer id) {
+        deptService.deleteById(id);
+        return Result.success();
+    }
+
+    /**
+     * 新增部门
+     */
+    @PostMapping("/depts")
+    public Result add(@RequestBody Dept dept) {
+        return Result.success();
+    }
+
 }
--- tlias-web-management/src/main/java/com/itheima/service/DeptService.java
+++ tlias-web-management/src/main/java/com/itheima/service/DeptService.java
@@ -5,8 +5,13 @@ import com.itheima.pojo.Dept;
 import java.util.List;
 
 public interface DeptService {
-    /**
-     *查询所有部门
+    /*
+     * 查询所有部门
      */
     List<Dept> findAll();
+
+    /*
+     * 根据id删除部门
+     */
+    void deleteById(Integer id);
 }
