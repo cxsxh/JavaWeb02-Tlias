32bacfb76a1c5e56451e7e59faffa4ac71e35770
cxsxh <207433274@qq.com>
2024-12-22 15:17:45 +0800

    Tlias-JavaWeb

--- tlias-web-management/src/main/java/com/itheima/controller/DeptController.java
+++ tlias-web-management/src/main/java/com/itheima/controller/DeptController.java
@@ -3,11 +3,13 @@ package com.itheima.controller;
 import com.itheima.pojo.Dept;
 import com.itheima.pojo.Result;
 import com.itheima.service.DeptService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RequestMapping("/depts")
 @RestController
 public class DeptController {
