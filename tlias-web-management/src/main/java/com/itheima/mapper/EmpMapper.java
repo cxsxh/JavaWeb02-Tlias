6ceab54a00fa87c02d6750f1b9d185d92635ea4c
cxsxh <207433274@qq.com>
2025-01-03 01:41:31 +0800

    Tlias-JavaWeb

--- tlias-web-management/src/main/java/com/itheima/mapper/EmpMapper.java
+++ tlias-web-management/src/main/java/com/itheima/mapper/EmpMapper.java
@@ -1,6 +1,10 @@
 package com.itheima.mapper;
 
+import com.itheima.pojo.Emp;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 
 /*
@@ -8,4 +12,17 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface EmpMapper {
+
+    /*
+     * 查询员工总数
+     */
+    @Select("select count(*) from emp e left join dept d on e.dept_id = d.id")
+    public Long count();
+
+    /*
+     * 查询员工列表
+     */
+    @Select("select e.*, d.name deptName from emp e left join dept d on e.dept_id = d.id " +
+            "order by e.update_time desc limit #{start}, #{pageSize}")
+    public List<Emp> list(Integer start, Integer pageSize);
 }
--- tlias-web-management/src/main/java/com/itheima/pojo/Emp.java
+++ tlias-web-management/src/main/java/com/itheima/pojo/Emp.java
@@ -20,4 +20,7 @@ public class Emp {
     private Integer deptId; //关联的部门ID
     private LocalDateTime createTime; //创建时间
     private LocalDateTime updateTime; //修改时间
+
+    //部门名称
+    private String deptName;
 }
