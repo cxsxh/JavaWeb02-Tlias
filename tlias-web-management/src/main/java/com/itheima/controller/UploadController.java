ce2325e3d13f8914bd9aa73e1592d7310c005dd1
cxsxh <207433274@qq.com>
2025-01-09 01:38:41 +0800

    Tlias-JavaWeb

--- tlias-web-management/src/main/java/com/itheima/controller/UploadController.java
+++ tlias-web-management/src/main/java/com/itheima/controller/UploadController.java
@@ -7,14 +7,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 @Slf4j
 @RequestMapping("/upload")
 @RestController
 public class UploadController {
 
     @PostMapping
-    public Result upload(String name, String age, MultipartFile file) {
+    public Result upload(String name, String age, MultipartFile file) throws IOException {
         log.info("文件上传：{},{},{}", name, age, file);
+        //获取原始文件名
+        String originalFilename = file.getOriginalFilename();
+
+        //新的文件名
+        String extension = originalFilename.substring(originalFilename.lastIndexOf('.'));
+        String newFileName = UUID.randomUUID().toString() + extension;
+
+        //保存文件
+        file.transferTo(new File("E:/视频/test/" + newFileName));
+
         return Result.success();
     }
 }
