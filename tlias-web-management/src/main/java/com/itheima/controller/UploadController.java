0e2f14e03fc3b4d7190b1882a8d557c54869a118
cxsxh <207433274@qq.com>
2025-01-13 15:21:56 +0800

    Tlias-JavaWeb

--- tlias-web-management/src/main/java/com/itheima/controller/UploadController.java
+++ tlias-web-management/src/main/java/com/itheima/controller/UploadController.java
@@ -1,22 +1,24 @@
 package com.itheima.controller;
 
 import com.itheima.pojo.Result;
+import com.itheima.utils.AliyunOSSOperator;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.UUID;
 
 @Slf4j
 @RequestMapping("/upload")
 @RestController
 public class UploadController {
 
-    @PostMapping
+    //本地磁盘存储方案
+    /*@PostMapping
     public Result upload(String name, String age, MultipartFile file) throws IOException {
         log.info("文件上传：{},{},{}", name, age, file);
         //获取原始文件名
@@ -30,5 +32,17 @@ public class UploadController {
         file.transferTo(new File("E:/视频/test/" + newFileName));
 
         return Result.success();
+    }*/
+
+    @Autowired
+    AliyunOSSOperator aliyunOSSOperator;
+
+    @SneakyThrows
+    @PostMapping
+    public Result upload(MultipartFile file) throws IOException {
+        log.info("文件上传：{}", file);
+        String url = aliyunOSSOperator.upload(file.getBytes(), file.getOriginalFilename());
+        log.info("文件上传成功，url：{}", url);
+        return Result.success(url);
     }
 }
