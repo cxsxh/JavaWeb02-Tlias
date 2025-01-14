613c34581afb23b02f3d91039383a1cc9d6341dc
cxsxh <207433274@qq.com>
2025-01-14 16:00:54 +0800

    Tlias-JavaWeb

--- tlias-web-management/src/main/java/com/itheima/service/ReportService.java
+++ tlias-web-management/src/main/java/com/itheima/service/ReportService.java
@@ -1,4 +1,11 @@
 package com.itheima.service;
 
+import com.itheima.pojo.JobOption;
+
 public interface ReportService {
+
+    /*
+     * 统计员工职位人数
+     */
+    JobOption getEmpJobData();
 }
--- tlias-web-management/src/main/java/com/itheima/service/impl/ReportServiceImpl.java
+++ tlias-web-management/src/main/java/com/itheima/service/impl/ReportServiceImpl.java
@@ -1,4 +1,31 @@
 package com.itheima.service.impl;
 
-public class ReportServiceImpl {
+import com.itheima.mapper.EmpMapper;
+import com.itheima.pojo.JobOption;
+import com.itheima.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ReportServiceImpl implements ReportService {
+
+    @Autowired
+    private EmpMapper empMapper;
+
+    /*
+     * 统计员工职位人数
+     */
+    @Override
+    public JobOption getEmpJobData() {
+        //1.调用mapper接口，获取统计数据
+        List<Map<String, Object>> list = empMapper.countEmpJobData();
+
+        //2.组装结果并返回
+        List<Object> jobList = list.stream().map(dataMap -> dataMap.get("pos")).toList();
+        List<Object> dataList = list.stream().map(dataMap -> dataMap.get("num")).toList();
+        return new JobOption(jobList, dataList);
+    }
 }
