de52e4c101480a2b3ae9f7b4a0e84a5992ba69a4
cxsxh <207433274@qq.com>
2025-01-20 00:36:26 +0800

    Tlias-JavaWeb

---
+++ springboot-aop-quickstart/src/main/java/com/itheima/aop/RecordTimeAspect.java
@@ -0,0 +1,25 @@
+package com.itheima.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class RecordTimeAspect {
+
+    @Around("execution(* com.itheima.service.impl.*.*(..))")
+    public Object recordTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long begin = System.currentTimeMillis();
+
+        Object result = joinPoint.proceed();
+
+        long end = System.currentTimeMillis();
+        log.info("方法 {} 执行耗时：{}ms", joinPoint.getSignature(), end-begin);
+
+        return result;
+    }
+}
