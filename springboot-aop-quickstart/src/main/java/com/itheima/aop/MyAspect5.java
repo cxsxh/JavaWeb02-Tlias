dbe01777f5e8d613ab722cedacb19e48964d7fc4
cxsxh <207433274@qq.com>
2025-01-22 14:12:07 +0800

    Tlias-JavaWeb

--- springboot-aop-quickstart/src/main/java/com/itheima/aop/MyAspect5.java
+++ springboot-aop-quickstart/src/main/java/com/itheima/aop/MyAspect5.java
@@ -2,22 +2,23 @@ package com.itheima.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-//@Aspect
-public class MyAspect2 {
+@Aspect
+public class MyAspect5 {
     //前置通知
     @Before("execution(* com.itheima.service.impl.*.*(..))")
     public void before(){
-        log.info("MyAspect2 -> before ...");
+        log.info("MyAspect4 -> before ...");
     }
 
     //后置通知
-    @After("execution(* com.itheima.service.impl.*.*(..))")
+    @After("execution(* com.itheima.service.impl..*(..))")
     public void after(){
-        log.info("MyAspect2 -> after ...");
+        log.info("MyAspect4 -> after ...");
     }
 }
