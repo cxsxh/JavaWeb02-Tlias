ee963bb742afc8c109d93e57d7bdbeeccd311daa
cxsxh <207433274@qq.com>
2025-01-22 23:08:56 +0800

    Tlias-JavaWeb

---
+++ tlias-web-management/src/test/java/com/itheima/ThreadLocalTest.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class ThreadLocalTest {
+    private static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+    public static void main(String[] args) {
+        threadLocal.set("hello");
+        System.out.println(threadLocal.get());
+    }
+}
